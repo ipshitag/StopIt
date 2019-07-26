c45360e5193eb5d7273b53c85c1bbed76be9d340
ipshitag <iuippi3@gmail.com>
2019-07-27 02:43:44 +0530

    Update res file

--- app/src/main/java/com/example/stopit/MainActivity.java
+++ app/src/main/java/com/example/stopit/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.animation.Animation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Animation atg;
+
         //IMPORT FONT
-        Typeface Mlight = Typeface.createFromAsset (getAssets(),"fonts/MLight.ttf");
-        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
-        Typeface MRegular = Typeface.createFromAsset(getAssets(),"fonts.MRegular");
+        //Typeface Mlight = Typeface.createFromAsset (getAssets(),"fonts/MLight.ttf");
+        //Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        //Typeface MRegular = Typeface.createFromAsset(getAssets(),"fonts.MRegular");
 
 
     }
