d21dfa64a7837bdc158a3aafb22adbeb0785a0a8
ipshitag <iuippi3@gmail.com>
2019-07-26 21:54:03 +0530

    Update res file

--- app/src/main/java/com/example/stopit/MainActivity.java
+++ app/src/main/java/com/example/stopit/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.stopit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //IMPORT FONT
+        Typeface Mlight = Typeface.createFromAsset (getAssets(),"fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(),"fonts.MRegular");
+
+
     }
 }
